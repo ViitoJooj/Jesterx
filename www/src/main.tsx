c3a90b3353af83c1ce737c889afb10e7062e6fce
ViitoJooj <joaovitor819oqueres@gmail.com>
2026-07-25 19:32:33 -0300

    feat: add React entry point with initial hello world

---
+++ www/src/main.tsx
@@ -0,0 +1,9 @@
+import "./styles.scss"
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <h1>OLÀ!</h1>
+  </StrictMode>,
+)
