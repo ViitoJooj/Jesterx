e235b008decd58688da35992283e39f7f7337525
ViitoJooj <joaovitor819oqueres@gmail.com>
2026-08-06 12:12:10 -0300

    add login DTOs (LoginRequest, LoginResponse)
    ViitoJooj

--- internal/port/http/dtos/auth_dto.go
+++ internal/port/http/dtos/auth_dto.go
@@ -13,3 +13,18 @@ type RegisterResponse struct {
 	Email     string `json:"email"`
 	CreatedAt string `json:"created_at"`
 }
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	Token       string `json:"token"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int    `json:"expires_in"`
+	UserUUID    string `json:"user_uuid"`
+	WebsiteUUID string `json:"website_uuid"`
+	Name        string `json:"name"`
+	Email       string `json:"email"`
+}
